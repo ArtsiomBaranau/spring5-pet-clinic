ac02c7c6cf3ad98a273e62c672dca8f973f9c9ec
Artsiom Baranau <artsiombaranau@gmail.com>
2021-02-13 17:59:24 +0300

    add initial project.

---
+++ src/test/java/com/gmail/artsiombaranau/spring5petclinic/Spring5PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.artsiombaranau.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
