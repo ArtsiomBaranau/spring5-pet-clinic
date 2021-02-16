13cbf03b7d6b5c3627bfddbcc2f59df12cbe0731
Artsiom Baranau <artsiombaranau@gmail.com>
2021-02-16 16:57:42 +0300

    add POJO's. close #2.

---
+++ src/main/java/com/gmail/artsiombaranau/spring5petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.gmail.artsiombaranau.spring5petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/gmail/artsiombaranau/spring5petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.gmail.artsiombaranau.spring5petclinic.model;
+
+public class Vet extends Person {
+}
