6817b906452c13fc144bd3b2020526153cedce84
Artsiom Baranau <artsiombaranau@gmail.com>
2021-02-19 15:03:31 +0300

    refactor to common crud interface. close #10

--- pet-clinic-data/src/main/java/com/gmail/artsiombaranau/spring5petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/gmail/artsiombaranau/spring5petclinic/services/VetService.java
@@ -2,13 +2,7 @@ package com.gmail.artsiombaranau.spring5petclinic.services;
 
 import com.gmail.artsiombaranau.spring5petclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
