4583ace157b827104204da8287f1b813bc652ae7
Artsiom Baranau <artsiombaranau@gmail.com>
2021-03-08 16:27:26 +0300

    add specialties service and its map impl. close #28

---
+++ pet-clinic-data/src/main/java/com/gmail/artsiombaranau/spring5petclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.gmail.artsiombaranau.spring5petclinic.services;
+
+import com.gmail.artsiombaranau.spring5petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
+
---
+++ pet-clinic-data/src/main/java/com/gmail/artsiombaranau/spring5petclinic/services/map/SpecialtiesServiceMap.java
@@ -0,0 +1,35 @@
+package com.gmail.artsiombaranau.spring5petclinic.services.map;
+
+import com.gmail.artsiombaranau.spring5petclinic.model.Speciality;
+import com.gmail.artsiombaranau.spring5petclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesServiceMap extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
