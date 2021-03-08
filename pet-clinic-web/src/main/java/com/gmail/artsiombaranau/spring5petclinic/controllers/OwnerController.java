46dc5150d8497dcb3fbfa74c47bb491c9bdfc4cd
Artsiom Baranau <artsiombaranau@gmail.com>
2021-03-08 17:58:09 +0300

    fixed broken links. close #42

--- pet-clinic-web/src/main/java/com/gmail/artsiombaranau/spring5petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/gmail/artsiombaranau/spring5petclinic/controllers/OwnerController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
     private final OwnerService ownerService;
@@ -15,9 +16,14 @@ public class OwnerController {
     }
 
 
-    @RequestMapping("/owners")
+    @RequestMapping({"", "/"})
     public String listOfOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwner() {
+        return "notimplemented";
     }
 }
