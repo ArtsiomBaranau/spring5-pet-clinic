474cfbc056e2dbfaa54759b4f2a0a609845d25ae
Artsiom Baranau <artsiombaranau@gmail.com>
2021-03-18 15:30:15 +0300

    add jpa service impl for vet. close #38

---
+++ pet-clinic-data/src/main/java/com/gmail/artsiombaranau/spring5petclinic/services/jpa/VetServiceJPA.java
@@ -0,0 +1,48 @@
+package com.gmail.artsiombaranau.spring5petclinic.services.jpa;
+
+import com.gmail.artsiombaranau.spring5petclinic.model.Vet;
+import com.gmail.artsiombaranau.spring5petclinic.repositories.VetRepository;
+import com.gmail.artsiombaranau.spring5petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VetServiceJPA implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetServiceJPA(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
