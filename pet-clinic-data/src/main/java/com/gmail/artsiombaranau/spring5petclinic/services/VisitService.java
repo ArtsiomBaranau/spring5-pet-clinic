23a95fd125584498d9f0406a0e8eda3d1efaa976
Artsiom Baranau <artsiombaranau@gmail.com>
2021-03-18 16:18:02 +0300

    add map service impl for visit. close #43

---
+++ pet-clinic-data/src/main/java/com/gmail/artsiombaranau/spring5petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.gmail.artsiombaranau.spring5petclinic.services;
+
+import com.gmail.artsiombaranau.spring5petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
