ecaa4aa40b2a33f5f47db12fa3831ead446d1d0a
Artsiom Baranau <artsiombaranau@gmail.com>
2021-03-31 16:12:29 +0300

    implement vets json endpoint. close #57

--- pet-clinic-web/src/main/java/com/gmail/artsiombaranau/spring5petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/gmail/artsiombaranau/spring5petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.gmail.artsiombaranau.spring5petclinic.controllers;
 
+import com.gmail.artsiombaranau.spring5petclinic.model.Vet;
 import com.gmail.artsiombaranau.spring5petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -13,9 +18,14 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets.html"})
+    @RequestMapping({"/vets", "/vets.html"})
     public String listOfVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON() {
+        return vetService.findAll();
     }
 }
